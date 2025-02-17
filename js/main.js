37d9294f5c00d801abff1db91136640fc2beccfc
josala7 <joosala4@gmail.com>
2025-02-17 05:17:46 +0200

    responsive design

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let header = document.querySelector("header");
+let menuIcon = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
